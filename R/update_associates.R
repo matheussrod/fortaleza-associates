63aa38341b8da9beac03d3f20cc772aebd104793
Matheus S. Rodrigues <matheus.srodr@gmail.com>
2022-03-31 02:43:46 -0300

    Add code to update historical associates data

---
+++ R/update_associates.R
@@ -0,0 +1,16 @@
+
+url <- "https://www.sociofortaleza.com.br/"
+html <- rvest::read_html(x = url)
+datetime <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+
+associates_text <- html |> 
+  rvest::html_node("strong") |> 
+  rvest::html_text()
+
+associates_number <- as.integer(sub(pattern = "\\.", replacement = "", x = associates_text))
+
+associates_new <- data.frame("data" = datetime, "associates" = associates_number)
+associates_old <- read.csv2(file = here::here("data/associates.csv"))
+associates <- rbind(associates_old, associates_new)
+
+write.csv2(x = associates, file = here::here("data/associates.csv"), quote = FALSE, row.names = FALSE)
